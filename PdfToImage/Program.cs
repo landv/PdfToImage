85d2b69622be4ade8f5c33c416d458eaba9271b7
landv <landvcn@qq.com>
2024-04-24 14:41:09 +0800

    one

---
+++ PdfToImage/Program.cs
@@ -0,0 +1,48 @@
+﻿using PdfiumViewer;
+
+using System;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+
+namespace PdfToImage
+{
+    class Program
+	{
+		static void Main(string[] args)
+		{
+			var projectDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+
+			try
+			{
+				using (var document = PdfDocument.Load(projectDir + @"\Samples\input.pdf"))
+				{
+					var pageCount = document.PageCount;
+
+					for (int i = 0; i < pageCount; i++)
+					{
+						var dpi = 300;
+						
+						using (var image = document.Render(i, dpi, dpi, PdfRenderFlags.CorrectFromDpi))
+						{
+							var encoder = ImageCodecInfo.GetImageEncoders()
+								.First(c => c.FormatID == ImageFormat.Jpeg.Guid);
+							var encParams = new EncoderParameters(1);
+							encParams.Param[0] = new EncoderParameter(
+								System.Drawing.Imaging.Encoder.Quality, 100L);
+
+							image.Save(projectDir + @"\Samples\output_" + i + ".jpg", encoder, encParams);
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			
+			Console.WriteLine("Press enter to continue...");
+			Console.ReadLine();
+		}
+	}
+}
